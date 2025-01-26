4fdfc3e40633925724ef2dcfdfbee002e45ec060
langzaigg <448489320@qq.com>
2025-01-26 09:19:13 +0800

    去掉了一些神秘功能，gpt不响应at全体成员的事件

--- ATRI/plugins/repo.py
+++ ATRI/plugins/repo.py
@@ -23,29 +23,29 @@ class Repo(Service):
         Service.__init__(self, "反馈", "向维护者发送消息")
 
 
-reporter = Repo().on_command("来杯红茶", "向维护者发送消息", aliases={"反馈", "报告"})
+# reporter = Repo().on_command("来杯红茶", "向维护者发送消息", aliases={"反馈", "报告"})
 
 
-@reporter.handle([Cooldown(120, prompt=_repo_flmt_notice)])
-async def _ready_repo(matcher: Matcher, args: Message = CommandArg()):
-    msg = args.extract_plain_text()
-    if msg:
-        matcher.set_arg("repo", args)
+# @reporter.handle([Cooldown(120, prompt=_repo_flmt_notice)])
+# async def _ready_repo(matcher: Matcher, args: Message = CommandArg()):
+#     msg = args.extract_plain_text()
+#     if msg:
+#         matcher.set_arg("repo", args)
 
 
-@reporter.got("repo", "需要反馈的内容呢？~")
-async def _deal_repo(
-    bot: Bot,
-    event: MessageEvent,
-    repo_msg: str = ArgPlainText("repo"),
-):
-    user_id = event.get_user_id()
-    repo_0 = REPO_FORMAT.format(user=user_id, msg=repo_msg)
+# @reporter.got("repo", "需要反馈的内容呢？~")
+# async def _deal_repo(
+#     bot: Bot,
+#     event: MessageEvent,
+#     repo_msg: str = ArgPlainText("repo"),
+# ):
+#     user_id = event.get_user_id()
+#     repo_0 = REPO_FORMAT.format(user=user_id, msg=repo_msg)
 
-    for superuser in BotSelfConfig.superusers:
-        try:
-            await bot.send_private_msg(user_id=superuser, message=repo_0)
-        except BaseException:
-            await reporter.finish("发送失败了呢...")
+#     for superuser in BotSelfConfig.superusers:
+#         try:
+#             await bot.send_private_msg(user_id=superuser, message=repo_0)
+#         except BaseException:
+#             await reporter.finish("发送失败了呢...")
 
-    await reporter.finish("吾辈的心愿已由咱转告维护者！")
+#     await reporter.finish("吾辈的心愿已由咱转告维护者！")
